34201881a2d9312d69b144f33184b8c45f28d2d2
Yoru6678 <111304459+Yoru6678@users.noreply.github.com>
2025-10-10 14:49:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     bot: {
         prefixe: '*', // Préfixe - Le texte avant les commandes
         buyer: '784847248433479710', // Buyer
-        couleur: '#6495ED', // Couleur HEX
+        couleur: '#36adfa', // Couleur HEX
         footer: 'Haruka Protect', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '2', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
